dd3ae834675bd1992a2b356f4776d079aaaea5d5
guptasomnath <131764381+guptasomnath@users.noreply.github.com>
2026-07-27 13:28:17 +0530

    add a option to fetch products by choosing multiple cateogry

--- api/src/controllers/product.controller.ts
+++ api/src/controllers/product.controller.ts
@@ -244,15 +244,26 @@ export const getProductList = asyncErrorHandler(
     const filterValues: any[] = [];
 
     if (req.query.category) {
-      const key = isNumber(req.query.category) ? "p.category_id" : "c.slug";
+      // const categories = req.query.category.split(",");
+      let keys = new Array();
+      if(Array.isArray(req.query.category)) {
+        req.query.category.forEach(item => {
+          keys.push(item);
+          filterValues.push(item);
+        })
+        keys = [...req.query.category];
+      } else {
+        keys = [req.query.category]
+      }
+      let placeholders = generatePlaceholders(1, keys.length);
+      placeholdernum += keys.length;
+      const key = isNumber(keys[0]) ? "p.category_id" : "c.slug";
 
       if (filter === "") {
-        filter = `WHERE ${key} = $${placeholdernum++}`;
+        filter = `WHERE ${key} IN ${placeholders}`;
       } else {
-        filter += ` AND ${key} = $${placeholdernum++}`;
+        filter += ` AND ${key} IN ${placeholders}`;
       }
-
-      filterValues.push(req.query.category);
     }
 
     if (req.query.sub_category) {
